5c7c572940b55c87e467ae22422d9c40118126af
Jeremy Kemp <jekemp@davidson.edu>
2023-09-20 15:32:59 -0400

    Description

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,9 @@ goggles.addEventListener("mouseout", function () {
     main.style.opacity = 0;
     });
 
+    const describe = "The inspiration for this desgin was based on the hundreds and hundreds of complaints I hear everyday in the pool about peoples goggles fogging up. Anti-fog technology, no matter how advanced has always been a bit of a joke in the swimming world that alot of us believe is just a sales tactic!";
+    console.log(describe);
+
 
 
 
